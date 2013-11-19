8244d1a92159b3826e661fee38f7f48b93818d4d
Jose Angel Concepcion <alu0100697414@ull.edu.es>
2013-11-19 19:53:52 +0000

    Modificado el archivo prct09.rb

--- lib/prct09.rb
+++ lib/prct09.rb
@@ -1,4 +1,10 @@
-require "prct09/version"
+require "./lib/prct09/version"
+require "./lib/prct09/matriz.rb"
+require "./lib/prct09/matriz_densa.rb"
+require "./lib/prct09/matriz_dispersa.rb"
+
+require "./lib/prct09/racional.rb"
+require "./lib/prct09/gcd.rb"
 
 module Prct09
   # Your code goes here...
